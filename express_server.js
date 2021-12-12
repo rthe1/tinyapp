ff636465cbff632576c1c94b90ca6f549bca20b0
Reshawn <reshawnhamilton97@gmail.com>
2021-12-12 23:15:35 +0000

    refactor for submission comments added advancded permissions added some route changes

--- express_server.js
+++ express_server.js
@@ -10,7 +10,7 @@ app.use(cookieSession({
 const bcrypt = require('bcryptjs');
 
 const PORT = 8080;
-
+// --HELPER FUNCTIONS
 const generateRandomString = () => {
   return Math.random().toString(36).substring(2, 8);
 };
@@ -35,12 +35,14 @@ const urlsForUsers = (database, id) => {
 
     }
 
-  } 
-return output
+  }
+  return output
 };
 
 app.set("view engine", "ejs");
 
+
+// --DATABASES
 const urlDatabase = {
   b6UTxQ: {
     longURL: "https://www.tsn.ca",
@@ -65,27 +67,48 @@ const userDatabase = {
   }
 }
 
-// POST SECTION
+// --POST SECTION
 
+// Adds new url to database
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
   let id = generateRandomString();
-  urlDatabase[id] = req.body.longURL;
+  if (!req.session.user_id) {
+    return res.status(403).send('You need to be logged in to add new links!')
+  }
+  urlDatabase[id] = {
+    longURL: req.body.longURL,
+    userID: req.session.user_id,
+  };
   res.redirect(`/urls/${id}`);         // Respond with 'Ok' (we will replace this)
 });
-
+// dDletes url
 app.post("/urls/:shortURL/delete", (req, res) => {
+  const isAuthourized = urlDatabase[req.params.shortURL].userID === req.session.user_id;
+
+  if (!isAuthourized) {
+    return res.status(403).send("You are not authorized to delete this link!")
+  }
 
   delete urlDatabase[req.params.shortURL]
   res.redirect("/urls")
 })
-
+// Edits urls
 app.post("/urls/:shortURL", (req, res) => {
 
-  urlDatabase[req.params.shortURL] = req.body.longURL
+  const isAuthourized = urlDatabase[req.params.shortURL].userID === req.session.user_id;
+
+  if (!isAuthourized) {
+
+    return res.status(403).send('YOu are not authorized to edit this link!')
+
+  }
+
+
+  urlDatabase[req.params.shortURL].longURL = req.body.longURL
   res.redirect("/urls")
 })
-
+// Add new user
 app.post("/register", (req, res) => {
 
   const id = generateRandomString()
@@ -117,7 +140,7 @@ app.post("/register", (req, res) => {
   res.redirect("/urls")
 
 })
-
+// Allows existing user to login
 app.post('/login', (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
@@ -137,7 +160,7 @@ app.post('/login', (req, res) => {
 
   res.redirect("/urls");
 })
-
+// Deletes cookie
 app.post('/logout', (req, res) => {
 
   delete req.session.user_id
@@ -150,10 +173,12 @@ app.post('/logout', (req, res) => {
 
 // GET SECTION
 
+// Home page
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  res.redirect("/urls");
 });
 
+// Register page
 app.get("/register", (req, res) => {
   const templateVars = {
     user: userDatabase[req.session.user_id],
@@ -162,6 +187,7 @@ app.get("/register", (req, res) => {
   res.render("register", templateVars)
 });
 
+// Login page
 app.get("/login", (req, res) => {
   const templateVars = {
     user: userDatabase[req.session.user_id],
@@ -171,7 +197,7 @@ app.get("/login", (req, res) => {
   res.render("login", templateVars)
 });
 
-
+// Displays URLS user owns
 app.get("/urls", (req, res) => {
 
 
@@ -190,6 +216,7 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+// Add URLS to datatbase
 app.get("/urls/new", (req, res) => {
   if (!req.session.user_id) {
     res.redirect("/login");
@@ -201,6 +228,7 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new", templateVars);
 });
 
+// Shortened link route
 app.get("/u/:shortURL", (req, res) => {
   // const longURL = ...
 
@@ -209,18 +237,18 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
+// URL edit page
 app.get("/urls/:shortURL", (req, res) => {
-  const isAuthourized = urlDatabase[shortURL].userID === req.session.user_id;
 
+  const isAuthourized = urlDatabase[req.params.shortURL].userID === req.session.user_id;
 
   if (!req.session.user_id || !isAuthourized) {
-    res.redirect("/login");
+    res.redirect("/urls");
   }
 
-  
   const templateVars = {
     shortURL: req.params.shortURL,
-    longURL: urlDatabase[req.params.shortURL],
+    longURL: urlDatabase[req.params.shortURL].longURL,
     user: userDatabase[req.session.user_id],
   };
   res.render("urls_show", templateVars);
